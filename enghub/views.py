b8159a2c4444649916d320ebb4658fec734aa487
kingsley <kinscoco9@gmail.com>
2025-08-19 18:56:36 +0100

    Added views.py with User and Task views

--- enghub/views.py
+++ enghub/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, permissions
+from .models import User, Task
+from .serializers import UserSerializer, TaskSerializer
+from .permissions import IsAdminOrReadOnly, IsTaskOwnerOrAdmin
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def get_permissions(self):
+        if self.action in ['create']:
+            return [permissions.IsAuthenticated(), IsAdminOrReadOnly()]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [permissions.IsAuthenticated(), IsTaskOwnerOrAdmin()]
+        return [permissions.IsAuthenticated()]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
